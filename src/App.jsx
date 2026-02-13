d034bf7b32bf77addc997540d998b97696c38ba8
Marcus <marcus.vfs2001@hotmail.com>
2026-02-12 22:37:10 -0300

    ajustando curva e iniciando refatoramento no css

--- src/App.jsx
+++ src/App.jsx
@@ -7,69 +7,73 @@ import { useState } from 'react';
 import relogio from './components/react-img/relogio.png';
 import DadosDetalhados from './components/dadosDetalhados/dados.jsx';
 import Footer from './components/footer/footer.jsx';
-import Apresentacao from './components/caixa de apresentação/apresentacao.jsx';
+import Apresentacao from './components/cabeca/apresentacao.jsx';
 
 function CorpoPadrao() {
   const [openModal, setOpenModal] = useState(false);
 
   return (
     <>
-    <div id='reader'>
+      <div>
+       
+          <div class="custom-shape-divider-bottom-1770849813" id="reader">
+           <Apresentacao />
 
-    </div>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
 
-    <div id='corpo'>
-          <DadosDetalhados />
-    </div>
-    <div id='pes'>
+              <path
+                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                class="shape-fill"
+              > </path>
+            </svg>
+          </div>
+     
 
-    </div>
 
+
+      </div>
+
+      <div id="corpo">
+        <DadosDetalhados />
+      </div>
+      <div id="pes">
+
+
+        
+
+        
+        
+        <CaixasDeSites
+          src={relogio}
+          alt="Reloginho Fofo"
+          onClick={() => setOpenModal(true)}
+          nome="Reloginho"
+          descricao="Um relogio muito fofo"
+        />
+        {
+          //
+          //        <div id="corpo">
+          //      <button onClick={() => setOpenModal(true)}>Abrir Modal</button>
+          //    <Modal
+          //    nome="marcus"
+          //  descricao="eu sou seu filho"
+          //isOpen={openModal}
+          //setOpenModal={() => setOpenModal(!openModal)}
+          //</div>></Modal>
+          //</div>
+        }
+      </div>
 
       <Cabeca />
       {
         // esse carroceu ta pronto, mas faz sentido usar? <Exibicao />}
       }
-      <section id="sessao">
-        <div class="custom-shape-divider-bottom-1770849813">
-      <Apresentacao />
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              class="shape-fill"
-            ></path>
-          </svg>
-          
-        </div>
-        <div id="corpo">
-          <CaixasDeSites
-            src={relogio}
-            alt="Reloginho Fofo"
-            onClick={() => setOpenModal(true)}
-            nome="Reloginho"
-            descricao="Um relogio muito fofo"
-          />
 
-          <button onClick={() => setOpenModal(true)}>Abrir Modal</button>
-
-          <Modal
-            nome="marcus"
-            descricao="eu sou seu filho"
-            isOpen={openModal}
-            setOpenModal={() => setOpenModal(!openModal)}
-          ></Modal>
-        </div>
-       
-      </section>
-
-
-        
-      
       <Footer />
     </>
   );
--- src/components/cabeca/apresentacao.jsx
+++ src/components/cabeca/apresentacao.jsx
@@ -1,9 +1,10 @@
 import pc from './pc.png';
+import '../cabeca/style.css';
 
 export default function Apresentacao() {
   return (
-    <div>
+    <div id="testando">
       <img src={pc} alt="Computador" />
     </div>
-    );
+  );
 }
