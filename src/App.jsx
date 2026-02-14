84552d552d4733db7f6347abd42bbb47e6dca737
Marcus <marcus.vfs2001@hotmail.com>
2026-02-13 21:30:13 -0300

    refatorando codigo e diminuindo linhas desnecessárias

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import Cabeca from './components/header';
 import CaixasDeSites from './components/caixasDeSites';
-import Exibicao from './components/carroceu/carroceu.jsx';
 import Modal from './components/caixaDescritiva/modal.jsx';
 import { useState } from 'react';
 import relogio from './components/react-img/relogio.png';
 import DadosDetalhados from './components/dadosDetalhados/dados.jsx';
 import Footer from './components/footer/footer.jsx';
-import Apresentacao from './components/cabeca/apresentacao.jsx';
+import pc from './components/react-img/pc.png';
+
 
 function CorpoPadrao() {
   const [openModal, setOpenModal] = useState(false);
@@ -15,39 +15,40 @@ function CorpoPadrao() {
   return (
     <>
       <div>
-       
-          <div class="custom-shape-divider-bottom-1770849813" id="reader">
-           <Apresentacao />
-
-            <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
-
-              <path
-                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-                class="shape-fill"
-              > </path>
-            </svg>
+        <div class="custom-shape-divider-bottom-1770849813" id="reader">
+          
+          <div id="caixa-do-pc">
+                <img src={pc} alt="Computador" />
+              </div>
+              
+          <div id="descricao-site-container">
+            <div id="caixinha-descritiva">
+              <p id="descricao-site">
+                site exlusivamente destina para links de sites produzidos por
+                marcus
+              </p>
+            </div>
           </div>
-     
-
-
-
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              class="shape-fill"
+            >
+              {' '}
+            </path>
+          </svg>
+        </div>
       </div>
 
       <div id="corpo">
         <DadosDetalhados />
       </div>
       <div id="pes">
-
-
-        
-
-        
-        
         <CaixasDeSites
           src={relogio}
           alt="Reloginho Fofo"
@@ -55,24 +56,9 @@ function CorpoPadrao() {
           nome="Reloginho"
           descricao="Um relogio muito fofo"
         />
-        {
-          //
-          //        <div id="corpo">
-          //      <button onClick={() => setOpenModal(true)}>Abrir Modal</button>
-          //    <Modal
-          //    nome="marcus"
-          //  descricao="eu sou seu filho"
-          //isOpen={openModal}
-          //setOpenModal={() => setOpenModal(!openModal)}
-          //</div>></Modal>
-          //</div>
-        }
       </div>
 
       <Cabeca />
-      {
-        // esse carroceu ta pronto, mas faz sentido usar? <Exibicao />}
-      }
 
       <Footer />
     </>
